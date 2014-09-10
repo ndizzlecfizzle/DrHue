a12116650e3d7b6cdb8ce3321fc52f82562e33df
ndizzlecfizzle <ndizzlecfizzle@gmail.com>
2014-09-10 15:03:13 -0700

    updated drhue script
    Signed-off-by: ndizzlecfizzle <ndizzlecfizzle@gmail.com>

---
+++ DRHUE_WIFI.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#A Fancy Script By Nate Carle
+#Updated 00.00.0000
+#DRHUE_WIFI
+
+if 
+
+wireless network is connected
+
+then
+
+show green light
+
+
+if
+
+Wireless network is disconnected
+
+then
+
+show red light
+
+
+if 
+
+wireless network is weak between (first) & (second)
+
+then
+
+show shades of yellow
