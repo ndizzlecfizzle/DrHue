d6bd503e5d6e90a366b2d76be9984d6ea6e9fcea
ndizzlecfizzle <ndizzlecfizzle@gmail.com>
2014-09-10 17:58:12 -0700

    added a file to test python scripts and learn from it
    Signed-off-by: ndizzlecfizzle <ndizzlecfizzle@gmail.com>

---
+++ Hello.py
@@ -0,0 +1,10 @@
+#test python script
+
+print 'Hello, Strange New Person!'
+
+title = raw_input('What is your title?\n')
+print '%s, that sounds professional' % title
+
+name = raw_input('And what is your name?\n')
+print 'Hi, %s.' % name
+
